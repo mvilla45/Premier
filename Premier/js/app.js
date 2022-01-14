0ac9bf32ef9d90c0088ffcd79cab8e34a8458ad2
Maximilien <97113090+mvilla45@users.noreply.github.com>
2022-01-14 13:59:16 +0100

    Add files via upload

---
+++ Premier/js/app.js
@@ -0,0 +1,10 @@
+const a1 = document.getElementById("a1").innerHTML
+const a2 =document.getElementById("a2").innerHTML
+const a3 =document.getElementById("a3").innerHTML
+const input1 =document.getElementById("input1").innerHTML
+
+let myFunction = 1
+
+function myFunction() {
+  document.getElementById("input1").innerHTML = 1;
+}
